73bab191fecfc7123c2e0d94a4136a56cd9e991a
JSJL29 <74869080+JSJL29@users.noreply.github.com>
2024-10-01 23:04:41 +0200

    Update vue.js

--- vue.js
+++ vue.js
@@ -3,12 +3,12 @@ const Gallery = {
     return {
       category: '',
       images: [
-        { index: 1, src: '../img/IMG_4894.JPG', category: 'Landscape' },
-        { index: 2, src: '../img/IMG_4895.JPG', category: 'Landscape' },
-        { index: 3, src: '../img/IMG_4896.JPG', category: 'Landscape' },
-        { index: 4, src: '../img/IMG_4897.JPG', category: 'Landscape' },
-        { index: 5, src: '../img/IMG_4922.JPG', category: 'Animal' },
-        { index: 6, src: '../img/IMG_4925.JPG', category: 'Animal' }]
+        { index: 1, src: 'img/IMG_4894.JPG', category: 'Landscape' },
+        { index: 2, src: 'img/IMG_4895.JPG', category: 'Landscape' },
+        { index: 3, src: 'img/IMG_4896.JPG', category: 'Landscape' },
+        { index: 4, src: 'img/IMG_4897.JPG', category: 'Landscape' },
+        { index: 5, src: 'img/IMG_4922.JPG', category: 'Animal' },
+        { index: 6, src: 'img/IMG_4925.JPG', category: 'Animal' }]
     };
   },
   computed: {
